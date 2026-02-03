bb4b362a8ff5871ec0eddb2e9e4897ff6a58412f
23f2003821 <23f2003821@ds.study.iitm.ac.in>
2026-02-03 12:38:54 +0530

    Milestone-PPA DB-Relationship

--- app.py
+++ app.py
@@ -1,16 +1,26 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
+from models import db
+import os
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'mad1-secret-key'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database/db.sqlite3'
+
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+
+# THIS LINE FIXES my ERROR of accessing issue
+os.makedirs(os.path.join(BASE_DIR, 'database'), exist_ok=True)
+
+DB_PATH = os.path.join(BASE_DIR, 'database', 'db.sqlite3')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + DB_PATH
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-db = SQLAlchemy(app)
+db.init_app(app)
 
 @app.route('/')
 def home():
     return "Placement Portal App - MAD I"
 
 if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
---
+++ models.py
@@ -0,0 +1,95 @@
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+db = SQLAlchemy()
+
+# -------------------
+# Admin Model
+# -----------------
+class Admin(db.Model):
+    __tablename__ = 'admins'
+
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(200), nullable=False)
+
+
+# -----------------
+# Company Model
+# -------------------
+class Company(db.Model):
+    __tablename__ = 'companies'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(150), nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    approved = db.Column(db.Boolean, default=False)
+    industry = db.Column(db.String(100))
+
+    jobs = db.relationship('Job', backref='company', lazy=True)
+
+
+# ---------------------
+# Student Model
+# -------------------
+class Student(db.Model):
+    __tablename__ = 'students'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(150), nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+
+    applications = db.relationship('Application', backref='student', lazy=True)
+
+    resume = db.Column(db.String(200))
+    skills = db.Column(db.String(300))
+
+
+# ------------------
+# Job / Placement Drive
+# -------------------
+class Job(db.Model):
+    __tablename__ = 'jobs'
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(150), nullable=False)
+    description = db.Column(db.Text)
+    status = db.Column(db.String(50), default='Pending')
+    salary = db.Column(db.Integer)
+    deadline = db.Column(db.Date)
+
+
+    company_id = db.Column(
+        db.Integer,
+        db.ForeignKey('companies.id'),
+        nullable=False
+    )
+
+    applications = db.relationship('Application', backref='job', lazy=True)
+
+
+# -------------------
+# Application (Bridge Table)
+# ------------------
+class Application(db.Model):
+    __tablename__ = 'applications'
+
+    id = db.Column(db.Integer, primary_key=True)
+    applied_on = db.Column(db.DateTime, default=datetime.utcnow)
+    status = db.Column(db.String(50), default='Applied')
+
+    student_id = db.Column(
+        db.Integer,
+        db.ForeignKey('students.id'),
+        nullable=False
+    )
+
+    job_id = db.Column(
+        db.Integer,
+        db.ForeignKey('jobs.id'),
+        nullable=False
+    )
+
+    __table_args__ = (
+        db.UniqueConstraint('student_id', 'job_id', name='unique_student_job'),
+    )
